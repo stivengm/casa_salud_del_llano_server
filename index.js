cdc3f82a515467cfd34c34e8fad64bc72a59a5a8
Stiven Gomez <stivengomez@soyyo.co>
2023-09-17 22:24:38 -0500

    Add get and post.

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 
 const app = express();
-
-const PORT = process.env.PORT ?? 3000;
+app.disable('x-powered-by');
 
 app.get('/', (req, res) => {
-    res.status(200).send('<h1>Mi página</h1>')
+    res.send('<h1>Mi página</h1>');
 });
 
+app.get('/stors', (req, res) => {
+    res.json({ test: "hola" });
+})
 
+app.post('/stors', (req, res) => {
+
+
+
+
+});
+
+app.use((req, res) => {
+    res.status(404).send({
+        "code": "F200",
+        "message": "No se ha encontrado el recurso",
+        "data": []
+    });
+});
+
+const PORT = process.env.PORT ?? 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
-})
+});
